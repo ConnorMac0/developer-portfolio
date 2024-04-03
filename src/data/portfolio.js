25b76eb9183fcf5f5b3912efd5390ad2b9b836ef
Connor Maclachlan <connormac000@gmail.com>
2024-04-03 13:28:25 -0700

    Update

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,37 +1,37 @@
 export default [
     {
         title: 'Chapter Tracker',
-        imgUrl: '/developer-portfolio/assets/ChapterTrackerImg.png',
+        imgUrl: './developer-portfolio/assets/ChapterTrackerImg.png',
         stack: ['Python','Pandas','Google Sheets'],
         link: 'https://github.com/ConnorMac0/ChapterTracker',
     },
     {
         title: 'Deep Fake Voice Detection',
-        imgUrl: '/developer-portfolio/assets/DeepFakeVoice.png',
+        imgUrl: './developer-portfolio/assets/DeepFakeVoice.png',
         stack: ['Python','Tensor Flow','Sklearn'],
         link: 'https://github.com/ConnorMac0/DeepFakeAudioDetection',
     },
     {
         title: 'Developer Portfolio',
-        imgUrl: '/developer-portfolio/assets/PortfolioImg.png',
+        imgUrl: './developer-portfolio/assets/PortfolioImg.png',
         stack: ['React', 'Tailwind','Vite'],
         link: '',
     },
     {
         title: 'BlackJack',
-        imgUrl: '/developer-portfolio/assets/BlackJackImg.png',
+        imgUrl: './developer-portfolio/assets/BlackJackImg.png',
         stack: ['Python', 'Py Torch','MatPlot'],
         link: 'https://github.com/ConnorMac0/BlackJack',
     },
     {
         title: 'Find My Shelter',
-        imgUrl: '/developer-portfolio/assets/FindMyShelterImg.png',
+        imgUrl: './developer-portfolio/assets/FindMyShelterImg.png',
         stack: ['HTML', 'PHP', 'Google APIs'],
         link: 'https://github.com/ConnorMac0/FindMyShelter',
     },
     {
         title: 'Tic-Tac-Toe',
-        imgUrl: '/developer-portfolio/assets/tictactoeImg.png',
+        imgUrl: './developer-portfolio/assets/tictactoeImg.png',
         stack: ['C++', 'Minimax', 'AI'],
         link: 'https://github.com/ConnorMac0/tic-tac-toe',
     },
